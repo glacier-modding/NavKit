ee907399fef734e1c4706f8c24ecead0e52d836c
Danny Bierek <dannybierek@gmail.com>
2024-06-30 23:49:15 -0700

    Add cmake project, vcpkg, and recast

---
+++ NavKit.cpp
@@ -0,0 +1,12 @@
+﻿// NavKit.cpp : Defines the entry point for the application.
+//
+
+#include "NavKit.h"
+#include <Recast.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello from NavKit with Recast." << endl;
+	return 0;
+}
