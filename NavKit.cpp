e1de2477c35f3c61f89c21a1cb39ae862d1a327e
Danny Bierek <dannybierek@gmail.com>
2024-07-01 01:34:03 -0700

    Add fastlz

--- NavKit.cpp
+++ NavKit.cpp
@@ -3,12 +3,15 @@
 
 #include "NavKit.h"
 #include <Recast.h>
-#include "SDL.h"
-#include "SDL_opengl.h"
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "External\fastlz\fastlz.h"
+#undef main
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello from NavKit with Recast." << endl;
+	cout << "Hello from NavKit with Recast, SDL, OpenGL, and fastlz." << endl;
 	return 0;
 }
