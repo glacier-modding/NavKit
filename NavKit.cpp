9a823dbdf3d4b266e6419d6b59750223d63cf9c4
Danny Bierek <dannybierek@gmail.com>
2024-07-05 23:58:27 -0700

    Add OpenGL and basic window

--- NavKit.cpp
+++ NavKit.cpp
@@ -12,6 +12,200 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello from NavKit with Recast, SDL, OpenGL, and fastlz." << endl;
+
+	// Init SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		printf("Could not initialise SDL.\nError: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	// Use OpenGL render driver.
+	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+
+	// Enable depth buffer.
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	// Set color channel depth.
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+	// 4x MSAA.
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	bool presentationMode = false;
+	Uint32 flags = SDL_WINDOW_OPENGL;
+	int width;
+	int height;
+	if (presentationMode)
+	{
+		// Create a fullscreen window at the native resolution.
+		width = displayMode.w;
+		height = displayMode.h;
+		flags |= SDL_WINDOW_FULLSCREEN;
+	}
+	else
+	{
+		float aspect = 16.0f / 9.0f;
+		width = rcMin(displayMode.w, (int)(displayMode.h * aspect)) - 80;
+		height = displayMode.h - 80;
+	}
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	int errorCode = SDL_CreateWindowAndRenderer(width, height, flags, &window, &renderer);
+
+	if (errorCode != 0 || !window || !renderer)
+	{
+		printf("Could not initialise SDL opengl\nError: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+	////if (!imguiRenderGLInit("DroidSans.ttf"))
+	////{
+	////	printf("Could not init GUI renderer.\n");
+	////	SDL_Quit();
+	////	return -1;
+	////}
+
+	float timeAcc = 0.0f;
+	Uint32 prevFrameTime = SDL_GetTicks();
+	int mousePos[2] = { 0, 0 };
+	int origMousePos[2] = { 0, 0 }; // Used to compute mouse movement totals across frames.
+
+	float cameraEulers[] = { 45, -45 };
+	float cameraPos[] = { 0, 0, 0 };
+	float camr = 1000;
+	float origCameraEulers[] = { 0, 0 }; // Used to compute rotational changes across frames.
+
+	float moveFront = 0.0f, moveBack = 0.0f, moveLeft = 0.0f, moveRight = 0.0f, moveUp = 0.0f, moveDown = 0.0f;
+
+	float scrollZoom = 0;
+	bool rotate = false;
+	bool movedDuringRotate = false;
+	float rayStart[3];
+	float rayEnd[3];
+	bool mouseOverMenu = false;
+
+	bool showMenu = !presentationMode;
+	bool showLog = false;
+	bool showTools = true;
+	bool showLevels = false;
+	bool showSample = false;
+	bool showTestCases = false;
+
+	// Window scroll positions.
+	int propScroll = 0;
+	int logScroll = 0;
+	int toolsScroll = 0;
+
+	string sampleName = "Choose Sample...";
+
+	//vector<string> files;
+	const string meshesFolder = "Meshes";
+	string meshName = "Choose Mesh...";
+
+	float markerPosition[3] = { 0, 0, 0 };
+	bool markerPositionSet = false;
+
+	//InputGeom* geom = 0;
+	//Sample* sample = 0;
+
+	const string testCasesFolder = "TestCases";
+	//TestCase* test = 0;
+
+	//BuildContext ctx;
+
+	//// Fog.
+	float fogColor[4] = { 0.32f, 0.31f, 0.30f, 1.0f };
+	glEnable(GL_FOG);
+	glFogi(GL_FOG_MODE, GL_LINEAR);
+	glFogf(GL_FOG_START, camr * 0.1f);
+	glFogf(GL_FOG_END, camr * 1.25f);
+	glFogfv(GL_FOG_COLOR, fogColor);
+
+	glEnable(GL_CULL_FACE);
+	glDepthFunc(GL_LEQUAL);
+
+	bool done = false;
+	while (!done)
+	{
+		// Handle input events.
+		int mouseScroll = 0;
+		bool processHitTest = false;
+		bool processHitTestShift = false;
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_KEYDOWN:
+				// Handle any key presses here.
+				if (event.key.keysym.sym == SDLK_ESCAPE)
+				{
+					done = true;
+				}
+				break;
+			case SDL_QUIT:
+				done = true;
+				break;
+
+			default:
+				break;
+			}
+		}
+		Uint32 time = SDL_GetTicks();
+		float dt = (time - prevFrameTime) / 1000.0f;
+		prevFrameTime = time;
+		// Clamp the framerate so that we do not hog all the CPU.
+		const float MIN_FRAME_TIME = 1.0f / 40.0f;
+		if (dt < MIN_FRAME_TIME)
+		{
+			int ms = (int)((MIN_FRAME_TIME - dt) * 1000.0f);
+			if (ms > 10) ms = 10;
+			if (ms >= 0) SDL_Delay(ms);
+		}
+
+		// Set the viewport.
+		glViewport(0, 0, width, height);
+		GLint viewport[4];
+		glGetIntegerv(GL_VIEWPORT, viewport);
+
+		// Clear the screen
+		glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glDisable(GL_TEXTURE_2D);
+		glEnable(GL_DEPTH_TEST);
+
+		glEnable(GL_FOG);
+		// Render GUI
+		glDisable(GL_DEPTH_TEST);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		//gluOrtho2D(0, width, 0, height);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+	}
+
+	//imguiRenderGLDestroy();
+
+	SDL_Quit();
+
+	//delete sample;
+	//delete geom;
+	printf("Got here!!");
+
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NavKit
 A tool to generate NAVP and AIRG files for Hitman: World of Assassination Scenes in the Glacier 2 Engine
+
+# Building instructions
+1. Open in Visual Studio
+1. Cmake should load
+1. In the x64 command prompt run:  
+`cmake --preset x64-debug .`
